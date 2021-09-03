603d6c05a39310636f37b63a158a83302a0062db
Bharat <magento@webindiainc.com>
2021-09-03 01:42:12 -0400

    Related product move bottom for mobile view

--- app/design/frontend/Mgs/claue_child/web/js/product/custom.js
+++ app/design/frontend/Mgs/claue_child/web/js/product/custom.js
@@ -5,14 +5,26 @@ require(['jquery'], function ($) {
         if ($('.case-block').length == 0) {
             return false;
         }
+        if ($('.related-products').length == 0) {
+            return false;
+        }
+        if (document.body.clientWidth < 768) 
+        {
 
-        if (document.body.clientWidth < 768) {
+            jQuery('.related-products').insertBefore('.shipping-section');
+
+        } else if (document.body.clientWidth > 767) {
+
+            jQuery('.related-products').insertAfter('.case-block');
+        }
+        if (document.body.clientWidth < 768) 
+        {
 
             jQuery('.case-block').insertAfter('#accordion');
 
         } else if (document.body.clientWidth > 767) {
 
-            jQuery('.case-block').insertAfter('.product_description');
+            jQuery('.case-block').insertAfter('.product_description_1');
         }
     }
     window.addEventListener('resize', function(event) {
