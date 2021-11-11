915f7e3c1aa8ec777459be24e23435ca1edc27f1
Bharat <magento@webindiainc.com>
2021-11-11 02:05:28 -0500

    add snapchat pixel

--- app/design/frontend/Mgs/claue/requirejs-config.js
+++ app/design/frontend/Mgs/claue/requirejs-config.js
@@ -1,11 +1,15 @@
 var config = {
     map: {
         '*':{
-              customjs: 'js/product/custom'
+              customjs: 'js/product/custom',
+              snapchatjs: 'js/snapchat'
          }
         },
     shim:{
      'customjs':{
+      deps: ['jquery']
+      },
+     'snapchatjs':{
       deps: ['jquery']
       }
      }
